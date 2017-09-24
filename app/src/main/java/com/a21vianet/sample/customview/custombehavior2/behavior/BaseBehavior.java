c9b8c6277fe32f63e20a3a46212964f8268d9fe2
QuincySx <772804430@qq.com>
2017-09-24 10:21:21 +0800

    优化第二种

--- app/src/main/java/com/a21vianet/sample/customview/custombehavior2/behavior/BaseBehavior.java
+++ app/src/main/java/com/a21vianet/sample/customview/custombehavior2/behavior/BaseBehavior.java
@@ -2,12 +2,15 @@ package com.a21vianet.sample.customview.custombehavior2.behavior;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.os.Handler;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewParentCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Scroller;
 
 import com.a21vianet.sample.customview.R;
 
@@ -19,12 +22,18 @@ import java.lang.ref.WeakReference;
 
 public class BaseBehavior extends CoordinatorLayout.Behavior {
     private WeakReference<View> mDependency;
+    private WeakReference<View> mView;
 
     private int mMinHeight = -1;
     private int mMaxHeight = -1;
 
     private int mCurrentImageHeight;
     private int mDefImageHeight;
+
+    private boolean isScrolling = false;
+
+    private Scroller mScroller;
+    private Handler mHandler;
 
     private ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1)
             .setDuration(50);
@@ -34,12 +43,15 @@ public class BaseBehavior extends CoordinatorLayout.Behavior {
 
     public BaseBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mScroller = new Scroller(context);
+        mHandler = new Handler();
     }
 
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
         if (dependency != null && dependency.getId() == R.id.layout_title) {
-            mDependency = new WeakReference(dependency);
+            mDependency = new WeakReference<>(dependency);
+            mView = new WeakReference<>(child);
             return true;
         }
         return super.layoutDependsOn(parent, child, dependency);
@@ -55,18 +67,23 @@ public class BaseBehavior extends CoordinatorLayout.Behavior {
     @Override
     public boolean onLayoutChild(CoordinatorLayout parent, View child, int layoutDirection) {
         if (mMaxHeight == -1) {
-            View layoutImgeView = ((LinearLayout) mDependency.get()).getChildAt(0);
+            View layoutImgView = getDependencyView().getChildAt(0);
             mMaxHeight = mDependency.get().getMeasuredHeight();
-            mMinHeight = mMaxHeight - layoutImgeView.getMeasuredHeight();
-            mDefImageHeight = layoutImgeView.getMeasuredHeight();
+            mMinHeight = mMaxHeight - layoutImgView.getMeasuredHeight();
+            mDefImageHeight = layoutImgView.getMeasuredHeight();
         }
 
-        mDependency.get().setTranslationY(0);
+        getDependencyView().setTranslationY(0);
 
         child.layout(0, 0, parent.getWidth(), parent.getHeight() - mMinHeight);
-        child.setTranslationY(mDependency.get().getMeasuredHeight() + mDependency.get().getTranslationY());
+        child.setTranslationY(getDependencyView().getMeasuredHeight() + getDependencyView().getTranslationY());
 
-        child.setOnTouchListener(new View.OnTouchListener() {
+        initChildTouchListener();
+        return true;
+    }
+
+    private void initChildTouchListener() {
+        getView().setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
@@ -77,7 +94,6 @@ public class BaseBehavior extends CoordinatorLayout.Behavior {
                 return false;
             }
         });
-        return true;
     }
 
     @Override
@@ -87,29 +103,41 @@ public class BaseBehavior extends CoordinatorLayout.Behavior {
 
     @Override
     public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
+        upSlide(dy, consumed);
+    }
+
+    private void upSlide(int dy, int[] consumed) {
         if (dy > 0) {
-            float measuredHeight = mDependency.get().getTranslationY();
-            if ((measuredHeight - dy) + mDependency.get().getMeasuredHeight() >= mMinHeight) {
-                mDependency.get().setTranslationY(measuredHeight - dy);
+            LinearLayout ImageLayout = (LinearLayout) getDependencyView().getChildAt(0);
+            ViewGroup.LayoutParams imageLayoutLayoutParams = ImageLayout.getLayoutParams();
+            mCurrentImageHeight = imageLayoutLayoutParams.height;
+
+            float measuredHeight = getDependencyView().getTranslationY();
+            if ((measuredHeight - dy) + getDependencyView().getMeasuredHeight() >= mMinHeight) {
+                getDependencyView().setTranslationY(measuredHeight - dy);
                 consumed[1] = dy;
             } else {
-                mDependency.get().setTranslationY(mMinHeight - mDependency.get().getMeasuredHeight());
+                getDependencyView().setTranslationY(mMinHeight - getDependencyView().getMeasuredHeight());
             }
         }
     }
 
     @Override
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        if (dyUnconsumed < 0) {
-            float measuredHeight = mDependency.get().getTranslationY();
-            if ((measuredHeight - dyUnconsumed) + mDependency.get().getMeasuredHeight() <= mMaxHeight) {
-                mDependency.get().setTranslationY(measuredHeight - dyUnconsumed);
-            } else {
-                mDependency.get().setTranslationY(0);
+        downSlide(dyUnconsumed);
+    }
 
-                LinearLayout ImageLayout = (LinearLayout) ((ViewGroup) mDependency.get()).getChildAt(0);
+    private void downSlide(int dy) {
+        if (dy < 0) {
+            float measuredHeight = getDependencyView().getTranslationY();
+            if ((measuredHeight - dy) + getDependencyView().getMeasuredHeight() <= mMaxHeight) {
+                getDependencyView().setTranslationY(measuredHeight - dy);
+            } else {
+                getDependencyView().setTranslationY(0);
+
+                LinearLayout ImageLayout = (LinearLayout) getDependencyView().getChildAt(0);
                 ViewGroup.LayoutParams imageLayoutLayoutParams = ImageLayout.getLayoutParams();
-                imageLayoutLayoutParams.height -= dyUnconsumed;
+                imageLayoutLayoutParams.height -= dy;
                 mCurrentImageHeight = imageLayoutLayoutParams.height;
                 ImageLayout.setLayoutParams(imageLayoutLayoutParams);
             }
@@ -131,7 +159,7 @@ public class BaseBehavior extends CoordinatorLayout.Behavior {
             return;
         }
         final int diffHeight = mCurrentImageHeight - mDefImageHeight;
-        final LinearLayout ImageLayout = (LinearLayout) ((ViewGroup) mDependency.get()).getChildAt(0);
+        final LinearLayout ImageLayout = (LinearLayout) getDependencyView().getChildAt(0);
         final ViewGroup.LayoutParams imageLayoutLayoutParams = ImageLayout.getLayoutParams();
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
@@ -142,5 +170,76 @@ public class BaseBehavior extends CoordinatorLayout.Behavior {
             }
         });
         valueAnimator.start();
+    }
+
+    @Override
+    public boolean onNestedPreFling(CoordinatorLayout coordinatorLayout, View child, View target, float velocityX, float velocityY) {
+        ViewParentCompat.onStartNestedScroll(coordinatorLayout, child, target, (int) velocityY);
+        return onUserStopDragging(velocityY);
+    }
+
+    private boolean onUserStopDragging(float velocity) {
+        ViewGroup dependencyView = getDependencyView();
+        float translateY = dependencyView.getTranslationY();
+        float minHeaderTranslate = -(dependencyView.getHeight() - dependencyView.getChildAt(1).getMeasuredHeight());
+
+        if (translateY == 0 || translateY == minHeaderTranslate) {
+            return false;
+        }
+
+        boolean targetState; // Flag indicates whether to expand the content.
+        if (Math.abs(velocity) <= 800) {
+            if (Math.abs(translateY) < Math.abs(translateY - minHeaderTranslate)) {
+                targetState = false;
+            } else {
+                targetState = true;
+            }
+            velocity = 800; // Limit velocity's minimum value.
+        } else {
+            if (velocity > 0) {
+                targetState = true;
+            } else {
+                targetState = false;
+            }
+        }
+
+        float targetTranslateY = targetState ? minHeaderTranslate : 0;
+        mScroller.startScroll(0, (int) translateY, 0, (int) (targetTranslateY - translateY), (int) (1000000 / Math.abs(velocity)));
+        mHandler.post(flingRunnable);
+        isScrolling = true;
+        return true;
+    }
+
+    private Runnable flingRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (mScroller.computeScrollOffset()) {
+                getDependencyView().setTranslationY(mScroller.getCurrY());
+                mHandler.post(this);
+            } else {
+                isScrolling = false;
+            }
+        }
+    };
+
+    private boolean canUp() {
+        final int measuredHeight = getDependencyView().getMeasuredHeight();
+        final int titleHeight = getDependencyView().getChildAt(1).getMeasuredHeight();
+        if (-(measuredHeight - titleHeight) >= getDependencyView().getTranslationY()) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean canDown() {
+        return false;
+    }
+
+    private ViewGroup getDependencyView() {
+        return (ViewGroup) mDependency.get();
+    }
+
+    private View getView() {
+        return mView.get();
     }
 }
